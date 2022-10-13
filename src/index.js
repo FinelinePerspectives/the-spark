67810a0df63193be3c7dc742661b92c60ff3ffe1
ryan_peterson <ryanjay333@gmail.com>
2022-10-13 11:01:35 -0400

    added contact form

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import 'fslightbox';
 import initTimeline from './js/intiTimeline';
 import initSwipers from './js/initSwipers';
 import initMap from './js/initMap';
+import initContactForm from './js/initContactForm';
 
 initTimeline();
 initSwipers();
 initMap();
+initContactForm();
--- src/js/initContactForm.js
+++ src/js/initContactForm.js
@@ -1,34 +1,37 @@
-const url = 'http://192.99.46.183/~zanutta/php/contact_me.php';
+const url = 'http://192.99.46.183/~thespark/php/contact_me.php';
 
 const submitBtn = document.querySelector('#submitBtn');
-const contactResponse = document.querySelector('#contactResponse');
-const formInputs = document.querySelectorAll('.contactPopup__input');
-const popupClose = document.querySelector('.contactPopup__close');
+const formResponse = document.querySelector('.form__response');
+const formInputs = document.querySelectorAll('.form-input');
 
 function initContactForm() {
-    submitBtn.addEventListener('click', () => submitContactForm());
+    submitBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        submitContactForm();
+    });
     formInputs.forEach(input => input.addEventListener('input', () => {
         input.classList.remove('input-error');
-        contactResponse.textContent = "";
-        contactResponse.classList.remove('active');
+        formResponse.classList.remove('active');
+        formResponse.textContent = "";
     }));
+
 }
 
 
 function postForm(data) {        
     $.post(url, data, function (data) {
-        contactResponse.textContent = 'Thank you! We will be in touch shortly.'
-        contactResponse.classList.add('active');
+        formResponse.textContent = 'Thank you! We will be in touch shortly.'
+        formResponse.classList.add('active');
 
         setTimeout(() => {
-            contactResponse.classList.remove('active');
-            contactResponse.textContent = "";
+            formResponse.classList.remove('active');
+            formResponse.textContent = "";
 
             formInputs.forEach(input => input.value = '');
         }, 3000);
     }).fail(err => {
-        contactResponse.textContent = 'Something went wrong...'
-        contactResponse.classList.add('active');
+        formResponse.textContent = 'Something went wrong...'
+        formResponse.classList.add('active');
         console.log(err);
     });
 }
@@ -36,43 +39,42 @@ function postForm(data) {
 function submitContactForm() {
     let formValid = true;
 
-    const userFirstName = document.querySelector(".contactPopup__input[name='firstName']");
-    const userLastName = document.querySelector(".contactPopup__input[name='lastName']");
-    const userEmail = document.querySelector(".contactPopup__input[name='email']");
-    const userPhone = document.querySelector(".contactPopup__input[name='phone']");
-    const userTypeOfProject = document.querySelector(".contactPopup__input[name='typeOfProject']");
-    const userHasDesignLayouts = document.querySelector(".contactPopup__input[name='designLayouts']");
-    const userFile = document.querySelector(".contactPopup__input[name='file']");
-    const userMessage = document.querySelector(".contactPopup__input[name='message']");
+    const userFirstName = document.querySelector(".form-input[name='firstName']");
+    const userLastName = document.querySelector(".form-input[name='lastName']");
+    const userEmail = document.querySelector(".form-input[name='email']");
+    const userPhone = document.querySelector(".form-input[name='phoneNumber']");
+    const userMessage = document.querySelector(".form-input[name='message']");
+    const userConsent = document.querySelector('#consent').checked;
 
-    const requiredFields = [userFirstName, userLastName, userEmail];
+    const requiredFields = [userFirstName, userLastName, userEmail, userPhone, userMessage];
 
     requiredFields.forEach(input => {
         if (input.value === "") {
             formValid = false;
             input.classList.add('input-error');
-            contactResponse.textContent = "Please fill out all required fields";
-            contactResponse.classList.add('active');
+            formResponse.textContent = "Please fill out all required fields";
+            formResponse.classList.add('active');
         }
     });
+    
+    if (formValid) {
+        if (!userConsent) {
+            formValid = false;
+            formResponse.textContent = "Please acknowledge the privacy policy";
+            formResponse.classList.add('active');
+        };
+    }
 
     if (formValid) {
         const formData = {
             userFirstName: userFirstName.value,
             userLastName: userLastName.value,
             userEmail: userEmail.value,
-            userPhone: userPhone.value === "" ? "No phone number provided" : userPhone.value,
-            userTypeOfProject: userTypeOfProject.value === "" ? "No project type given" : userTypeOfProject.value,
-            userHasDesignLayouts: userHasDesignLayouts.value === "" ? "No design layouts or blueprints provided" : userHasDesignLayouts.value,
-            userFile: userFile.value,
-            userMessage: userMessage.value === "" ? "No message provided" : userMessage.value
+            userPhone: userPhone.value,
+            userMessage: userMessage.value
         }
 
         postForm(formData);
-
-        setTimeout(() => {
-            popupClose.click();
-        }, 2500);
     }
 }
 
