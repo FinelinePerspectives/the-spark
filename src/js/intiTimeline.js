2676ed61b5a3cb48a1473d14708e7df40ed11239
ryan_peterson <ryanjay333@gmail.com>
2022-10-14 10:54:37 -0400

    revised timeline styles for mobile and desktop

--- src/js/intiTimeline.js
+++ src/js/intiTimeline.js
@@ -1,10 +1,31 @@
 function initTimeline() {
-    const completedPhases = [1, 2];
+    const currentPhase = parseInt(document.querySelector('#currentPhase').value);
     const markers = document.querySelectorAll('.timeline__marker');
-    const progress = (completedPhases.length / 6) * 100;
+    let progress = 0;
+
+    if (currentPhase === 1) progress = 12.5;
+    if (currentPhase === 2) progress = 32.5;
+    if (currentPhase === 3) progress = 50;
+    if (currentPhase === 4) progress = 70;
+    if (currentPhase === 5) progress = 90;
+    if (currentPhase === 6) progress = 100;
+
     const progressBar = document.querySelector('#progressBar');
-    progressBar.style.width = `${progress}%`
-    completedPhases.forEach((phase, i) => markers[i].classList.add('complete'));
+
+    function setProgress() {
+        if (window.innerWidth <= 1024) {
+            progressBar.style.height = `${progress}%`;
+            progressBar.style.width = '20px';
+        } else {
+            progressBar.style.width = `${progress}%`;
+            progressBar.style.height = '20px';
+        }
+    }
+
+    window.addEventListener('load', () => setProgress());
+    window.addEventListener('resize', () => setProgress());
+    
+    markers.forEach((marker, i) => i + 1 <= currentPhase && marker.classList.add('complete'));
 }
 
 export default initTimeline;
