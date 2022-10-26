0cd9c96c3f02245ff5502dfba35d6e4043c9f0b9
Ryan Peterson <rpeterson@finelineperspectives.com>
2022-10-26 14:09:18 -0400

    added function faq section

--- src/js/initFaqs.js
+++ src/js/initFaqs.js
@@ -1,14 +1,21 @@
 function initFaqs() {
     const faqBtns = document.querySelectorAll('.btn-faq');
+    const faqItems = document.querySelectorAll('.faq__item');
 
-        faqBtns.forEach(btn => btn.addEventListener('click', (e) => {
-            const selectedFaq = btn.getAttribute('data-faq');
+        faqBtns.forEach(btn => btn.addEventListener('click', () => {
+            const selectedSection = btn.getAttribute('data-faq');
     
-            const selectedBtn = document.querySelector(`.btn-faq[data-faq='${selectedFaq}']`);
-            const selectedFaqSection = document.querySelector(`.faq__section[data-faq='${selectedFaq}']`);
+            const selectedBtn = document.querySelector(`.btn-faq[data-faq='${selectedSection}']`);
+            const selectedFaqSection = document.querySelector(`.faq__section[data-faq='${selectedSection}']`);
             const isActive = btn.classList.contains('active');
     
             if (!isActive) {
+                const sectionItems = selectedFaqSection.querySelectorAll('.faq__item');
+                sectionItems.forEach((item, i) => {
+                    item.classList.remove('active')
+                });
+                sectionItems[0].classList.add('active');
+
                 const activeBtn = document.querySelector('.btn-faq.active');
                 const activeFaqSection = document.querySelector('.faq__section.active');
     
@@ -19,6 +26,18 @@ function initFaqs() {
                 selectedFaqSection.classList.add('active');
             }
         }));
+
+        faqItems.forEach(item => {
+            item.addEventListener('click', () => {
+                const selectedFaq = item;
+                const isActive = item.classList.contains('active');
+
+                if (!isActive) {
+                    faqItems.forEach(item => item.classList.remove('active'));
+                    selectedFaq.classList.add('active');
+                }
+            })
+        })
 }
 
 export default initFaqs;
